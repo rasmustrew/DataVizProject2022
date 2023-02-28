64a009060d7c2d71c3217c6700278417972c96d8
locpe <locpeter@gmail.com>
2023-02-28 17:03:26 -0500

    Quantile splits now computing percentiles only with unique data point values

--- src/algorithms/quantile_splits.js
+++ src/algorithms/quantile_splits.js
@@ -1,12 +1,13 @@
 import ProportionateSplitMapper from "../mappings/proportionate_split_mapping";
 
 export function quantile_splits(sorted_data, args) {
+    let unique_data = [...new Set(sorted_data)]
     let number_of_splits = args["clusters"]
-    let n_elements = Math.floor(sorted_data.length / number_of_splits)
+    let n_elements = Math.floor(unique_data.length / number_of_splits)
     let split_points = []
     for (let i = 1; i < number_of_splits; i++) {
-        let left_border_value = sorted_data[i * n_elements]
-        let right_border_value = sorted_data[i * n_elements + 1]
+        let left_border_value = unique_data[i * n_elements]
+        let right_border_value = unique_data[i * n_elements + 1]
         let split_point = left_border_value + ((right_border_value - left_border_value) / 2)
         split_points.push(split_point)
     }
