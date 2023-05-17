8d2839baa16c084374acb3fb71592ff8e9be3702
locpe <locpeter@gmail.com>
2023-05-17 17:32:22 -0400

    Tick marks and bug fix for beeswarm.js

--- src/plots/beeswarm.js
+++ src/plots/beeswarm.js
@@ -3,9 +3,14 @@ import CompositeMapper from "../mappings/composite_mapping";
 import * as d3 from "d3";
 import {averageHeight, distortion} from "../benchmarks/benchmarks";
 import LinearMapper from "../mappings/linear_mapping";
+import ProportionateRangeMapper from "../mappings/proportionate_split_mapping";
+import SegmentScreenMapper from "../mappings/segment_screen_mapping";
+import {ExtendedWilkinson} from "../algorithms/extended_wilkinsons";
 
 
 export default class Beeswarm {
+    tick_spacing = 50
+    use_density_cues = true
 
     constructor(chart_ref, data, dimension, raw_mapper) {
         let plot = document.querySelector(chart_ref)
@@ -16,13 +21,23 @@ export default class Beeswarm {
         let width = plot.clientWidth - margin.left - margin.right;
         let height = plot.clientHeight - margin.top - margin.bottom;
         this.height = height
-        let screen_mapper = new ScreenMapper(raw_mapper.get_output_space_ranges(), [0, width], buffer_size)
-        let mapper = new CompositeMapper([raw_mapper, screen_mapper])
+        let mapper
+        if (raw_mapper instanceof ProportionateRangeMapper) {
+            mapper = new SegmentScreenMapper(raw_mapper, [0, width], buffer_size)
+        } else {
+            let output_ranges = raw_mapper.get_output_space_ranges()
+            let screen_mapper = new ScreenMapper(output_ranges, [0, width], buffer_size)
+            mapper = new CompositeMapper([raw_mapper, screen_mapper])
+        }
         this.mapper = mapper
         this.sorted_data = [...data]
         this.sorted_data.sort(function (a, b) {
             return a[dimension] - b[dimension];
         });
+        let input_ranges = mapper.get_input_space_ranges()
+        this.data_range_length = input_ranges[input_ranges.length - 1][1] - input_ranges[0][0]
+        this.tick_spacing = (1 - parseInt(d3.select("#tick_density_argument input").property("value")) / 100) ** 2 * Math.min(height, width) / 2
+
 
         // append the svg object to the body of the page
         const svg = d3.select(chart_ref).append("svg")
@@ -40,18 +55,32 @@ export default class Beeswarm {
 
         this.runBenchmarks()
     }
+
     create_single_beeswarm_plot(svg, data, mapper, index, dimension) {
         const input_range = mapper.get_input_space_ranges()[index]
         const output_range = mapper.get_output_space_ranges()[index]
+        const tile_width = output_range[1] - output_range[0]
         const x = d3.scaleLinear()
             .range(output_range)
             .domain(input_range)
         // .padding(1);
 
+        // Wilkinson ticks
+        let density_cue_x = 0.5 + (input_range[1] - input_range[0]) / this.data_range_length;
+        const no_x_ticks = Math.floor(tile_width * (this.use_density_cues ? density_cue_x : 1) / this.tick_spacing)
+        let ticks = ExtendedWilkinson(input_range, no_x_ticks).ticks
+
+        if (ticks === undefined) ticks = []
+        ticks = ticks.filter(tick => tick > input_range[0] && tick < input_range[1])
+        let tick_format = Intl.NumberFormat("en-GB", { maximumSignificantDigits: 4 })
+
         let axis_x = svg.append('g')
             .attr("class", "x_axis")
             .attr("transform", `translate(0, ${this.height})`)
-            .call(d3.axisBottom(x))
+            .call(d3.axisBottom(x)
+                .tickValues((tile_width >= this.tick_spacing) ? ticks : [])
+                .tickFormat((tick) => tick_format.format(tick))
+            )
             .selectAll("text")
             .attr("transform", "translate(-10,0)rotate(-45)")
             .style("text-anchor", "end");
